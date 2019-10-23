19484f0852aee87b6493d5f00ea478ea467a6f9e
Vanessa Sochat <vsochat@stanford.edu>
2019-10-23 15:10:31 -0400

    adding skeleton files
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nushell Plugin Python
+
+This will be a python module to allow for easy creation of plugins. It's
+already fairly easy, but this can improve upon that.
+
+**under development**
+
+## License
+
+This code is licensed under the MPL 2.0 [LICENSE](LICENSE).
+
+## Help and Contribution
+
+Please contribute to the package, or post feedback and questions as <a href="https://github.com/vsoch/nushell-plugin-python" target="_blank">issues</a>.
---
+++ nushell/version.py
@@ -0,0 +1,18 @@
+
+# Copyright (C) 2019 Vanessa Sochat.
+
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+__version__ = "0.0.0"
+AUTHOR = 'Vanessa Sochat'
+AUTHOR_EMAIL = 'vsochat@stanford.edu'
+NAME = 'nushell'
+PACKAGE_URL = "http://www.github.com/vsoch/nushell-python-plugin"
+KEYWORDS = 'nushell, plugin, nu'
+DESCRIPTION = "Python module to easily create nushell plugins"
+LICENSE = "LICENSE"
+
+INSTALL_REQUIRES = (
+)
